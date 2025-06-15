3e8c977a10111b174a2de1d3d76dcdb87b4b13b9
Digiweb <info@digiweb.be>
2025-06-15 19:02:18 +0200

    Remove elem from mobile

--- student3/js/student3.js
+++ student3/js/student3.js
@@ -3,7 +3,7 @@
     R-number: r1012723  */
 
 document.addEventListener('DOMContentLoaded', function() {
-    const API_BASE_URL = 'http://127.0.0.1:8000';
+    const API_BASE_URL = 'https://fullstack-2025-backend.ashyisland-75fa9c90.westeurope.azurecontainerapps.io';
 
     async function loadTeamMembers() {
         try {
